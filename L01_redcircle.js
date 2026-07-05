c157a080db9fe5338e3fd423d40dcc8ee938967a
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-07-05 04:20:44 +0000

    lesson one

--- L01_redcircle.js
+++ L01_redcircle.js
@@ -1,11 +1,15 @@
 function setup(){
     createCanvas(600, 400);
-    background(220);
+    background('grey'); // 220
 }
 
 function draw(){
-    fill(255, 0, 0);
-    ellipse(300,200,200,200);
-    
+    fill('red'); // 255,0,0
+    noStroke(); // no outline
+    //      x    y   w   h
+    ellipse(300,100,100,100);
+    fill('orange');
+    ellipse(300,200,100,100);
+
     // Todo: Challenge 3 Traffic Light
 }
