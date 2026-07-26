022a0d281428faf62db0fa0e1e07aa807e05ee5c
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-07-26 04:16:42 +0000

    Jul 26, 2026, 4:16 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -34,7 +34,7 @@ function setup() {
 // }
 
 function mousePressed() {
-    shapeColor = color(random(0,255));
+    shapeColor = color(random(0,255), random(0,255), random(0,255));
 }
 
 function mouseReleased() {
