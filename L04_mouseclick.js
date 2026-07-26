9ec63f4cd0fb48aff2f03f0bbe1f4137b128b50b
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-07-26 04:10:43 +0000

    Jul 26, 2026, 4:10 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -13,23 +13,23 @@ function setup() {
     //noLoop(); // for lesson 4 no need
 }
 
-function draw() {
-    // let xpos = 350;
-    // let ypos = 50;
-    // let ggColor = 0;
+// function draw() {
+//     // let xpos = 350;
+//     // let ypos = 50;
+//     // let ggColor = 0;
 
-    // for (let num=1; num<4; num++) {
-    //     fill(0, ggColor, 0);
-    //      //     x     y    diameter
-    //     circle(xpos, ypos, 50);
-    //     xpos = xpos-50;
-    //     ypos = ypos+50;
-    //     ggColor = ggColor + 50;
-    // }
+//     // for (let num=1; num<4; num++) {
+//     //     fill(0, ggColor, 0);
+//     //      //     x     y    diameter
+//     //     circle(xpos, ypos, 50);
+//     //     xpos = xpos-50;
+//     //     ypos = ypos+50;
+//     //     ggColor = ggColor + 50;
+//     // }
 
-    fill(shapeColor);
-    circle(width/2, height/2, 200);
-}
+//     fill(shapeColor);
+//     circle(width/2, height/2, 200);
+// }
 
 function mousePressed() {
     shapeColor = color(random(0,255));
