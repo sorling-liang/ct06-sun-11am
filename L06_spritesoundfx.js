1e696fdd53db2c5487195a70bc669dc8ff4d376a
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-08-16 04:10:27 +0000

    Aug 16, 2026, 4:10 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -10,7 +10,7 @@ function preload() {
     pico = loadImage("assets/pico-a.png");
 
     bgm = loadSound("assets/bossaNova.mp3");
-    sfx = XXXX
+    sfx = loadSound("assets/pop.mp3");
 }
 
 function setup() {
