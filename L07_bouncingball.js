1a3cc052f708ef55c49c13b13ec31aafe2068b7b
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-08-23 03:28:27 +0000

    Aug 23, 2026, 3:28 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,49 +1,70 @@
-// write your codes here
-let pico;
-let xpos, ypos;
-let bgm, sfx;
-let picoWidth;
 
-function preload() {
-    // any media, image or sound must load here
-    pico = loadImage("assets/pico-a.png");
-    sfx = loadSound("assets/pop.mp3");
-}
 
-function setup() {
-    createCanvas(600,600);
-    background("lightblue");
-    xpos = width/2;
-    ypos = height/2;
-    picoWidth = 110;
-}
 
-function draw() {
-    background("lightblue"); // erase my trail
-    imageMode(CENTER);
-    //          x    y     w          h
-    image(pico, xpos,ypos, picoWidth, 133);
 
-    if (keyIsDown(RIGHT_ARROW)) {
-        picoWidth = 110; // revert
-        xpos = xpos + 2;
-    }
-    if (keyIsDown(LEFT_ARROW)) {
-        xpos = xpos - 2;
-    }
-    if (keyIsDown(UP_ARROW)) {
-        ypos = ypos - 2;
-    }      
-    if (keyIsDown(DOWN_ARROW)) {
-        ypos = ypos + 2;
-    }
 
-    xpos = constrain(xpos, 0+55, width-55);
-    ypos = constrain(ypos, 0+65, height-65);    
-}
 
-function keyPressed() {
-    if (keyCode === 32) {
-        picoWidth = 55; // half-sized
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// recap
+// // write your codes here
+// let pico;
+// let xpos, ypos;
+// let bgm, sfx;
+// let picoWidth;
+
+// function preload() {
+//     // any media, image or sound must load here
+//     pico = loadImage("assets/pico-a.png");
+//     sfx = loadSound("assets/pop.mp3");
+// }
+
+// function setup() {
+//     createCanvas(600,600);
+//     background("lightblue");
+//     xpos = width/2;
+//     ypos = height/2;
+//     picoWidth = 110;
+// }
+
+// function draw() {
+//     background("lightblue"); // erase my trail
+//     imageMode(CENTER);
+//     //          x    y     w          h
+//     image(pico, xpos,ypos, picoWidth, 133);
+
+//     if (keyIsDown(RIGHT_ARROW)) {
+//         picoWidth = 110; // revert
+//         xpos = xpos + 2;
+//     }
+//     if (keyIsDown(LEFT_ARROW)) {
+//         xpos = xpos - 2;
+//     }
+//     if (keyIsDown(UP_ARROW)) {
+//         ypos = ypos - 2;
+//     }      
+//     if (keyIsDown(DOWN_ARROW)) {
+//         ypos = ypos + 2;
+//     }
+
+//     xpos = constrain(xpos, 0+55, width-55);
+//     ypos = constrain(ypos, 0+65, height-65);    
+// }
+
+// function keyPressed() {
+//     if (keyCode === 32) {
+//         picoWidth = 55; // half-sized
+//     }
+// }
