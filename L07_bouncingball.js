375d62758f1ac72857fc2e76e058d79708866337
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-08-23 03:10:44 +0000

    Aug 23, 2026, 3:10 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -4,7 +4,9 @@ let xpos, ypos;
 let bgm, sfx;
 
 function preload() {
-
+    // any media, image or sound must load here
+    pico = loadImage("assets/pico-a.png");
+    sfx = loadSound("assets/pop.mp3");
 }
 
 function setup() {
