d78b561bf0ea833cb053d272ac365cc4286afd64
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-08-23 04:08:06 +0000

    Aug 23, 2026, 4:08 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -22,6 +22,7 @@ function draw() {
 
     if (ballX > width) {
         ballSpeedX = -1.2 * ballSpeedX; // 2 * -1 = -2
+        //     R              G  
         fill( random(0, 255), 0, random(0, 255), random(10,55) );
     }
     if (ballX < 0) {
