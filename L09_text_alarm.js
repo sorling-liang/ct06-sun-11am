14c33838cc3192842cb09a2de1370a2c7f8dda19
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-09-20 03:33:17 +0000

    Sep 20, 2026, 3:33 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -14,7 +14,7 @@ function draw() {
     let sec = second();
     fill(0); // black text
     textSize(72);
-    text(nf(hh,2) + ":" + ":" + nf(sec,2), width/2, height/2);
+    text(nf(hh,2) + ":" + nf(mins,2) + ":" + nf(sec,2), width/2, height/2);
 }
 
 // CCS
