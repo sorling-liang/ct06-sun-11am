2eacbda393666400a623091e71b0d10af17e487a
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-09-20 04:10:02 +0000

    Sep 20, 2026, 4:10 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -39,6 +39,7 @@ function updateCountdown() {
     if (countdown === 0) {
         clearInterval(objectId); // delete the object
         bgColor = "red";
+        bg
     } 
     else {
         countdown = countdown - 1; // reduce by 1
